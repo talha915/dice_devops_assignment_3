9ca38988afca09b14c5cbbc2ef3634096ae38fcb
unknown <talhazafar915426@gmail.com>
2024-07-22 01:16:26 +0200

    added CI.yml lint fixes

--- app/index.py
+++ app/index.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
